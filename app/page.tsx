21ae674bcba20d08492cd3d63d895ff77a906f7c
Samuel Riester <riestersamuel@icloud.com>
2024-09-30 20:18:13 +0200

    configured shadcn ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">  
         <Image
           className="dark:invert"
           src="https://nextjs.org/icons/next.svg"
@@ -48,6 +49,7 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+       <Button>I'm the first shadcn component, a button</Button>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
